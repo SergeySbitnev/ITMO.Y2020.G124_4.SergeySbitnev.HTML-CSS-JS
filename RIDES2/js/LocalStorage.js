8890c92e83525e3894e39cbfe42c5db1c9965016
Sergey Sbitnev <sergeysbitnev@mail.ru>
2020-11-30 18:57:14 +0300

    30.11.2020 18:56

--- RIDES2/js/LocalStorage.js
+++ RIDES2/js/LocalStorage.js
@@ -1,3 +1,32 @@
-document.querySelector('.storage').onclick = () =>{
-    var name = document.getElementsByClassName('#name');
-};
+function doFirst(){
+    var button = document.getElementById('button');
+    button.addEventListener("click", save, false);
+    display();
+
+}
+
+function save(){
+    var name = document.getElementById('name').value;
+    var mail = document.getElementById('mail').value;
+    var phone = document.getElementById('phone');
+    var comments = document.getElementById('comments');
+    sessionStorage.setItem(name, mail);
+    display();  
+    name.value="";
+    mail.value="";
+    phone.value="";
+    comments.value="";
+    
+}
+
+function display(){
+    var display_data = document.getElementById('display_data');
+    display_data.innerHTML="";
+    for(var i=0;  i < sessionStorage.length; i++){
+        var a = sessionStorage.name(i);
+        var b = sessionStorage.getItem(a);
+        display_data.innerHTML += a + " - " + b + "<br>";
+    }
+}
+
+window.addEventListener("load", doFirst, false);
